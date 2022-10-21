7938a096e8ac18245c414d9ed2eaa1f0c47cfcb8
Vinícius Santos <vinicius.santos.oliv3ira@gmail.com>
2022-10-20 23:30:37 -0300

    salvando os dados do usuario logado no localStorage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -31,7 +31,7 @@ export class HomePage {
     this.auth.authenticate(this.creds)
       .subscribe(
         response => {
-          console.log(response.headers.get('Authorization'));
+          this.auth.successfullLogin(response.headers.get('Authorization'));
           this.navCtrl.setRoot(`CategoriasPage`);
         },
         error => {}
