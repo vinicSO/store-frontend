cb79fb7a59791a672cf6eb78dd0ebad6e9b9b2c9
Vinícius Santos <vinicius.santos.oliv3ira@gmail.com>
2022-10-20 21:27:06 -0300

    mostrando as categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-  baseUrl: "http://localhost:8081"
+  baseUrl: "http://localhost:8081",
+  bucketBaseUrl: "https://bucket-super-store.s3.sa-east-1.amazonaws.com"
 }
--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
+import { CategoriaDTO } from '../../models/categoria.dto';
 import { CategoriaService } from '../../service/domain/catergoria.service';
 
 /**
@@ -16,13 +18,17 @@ import { CategoriaService } from '../../service/domain/catergoria.service';
 })
 export class CategoriasPage {
 
+  bucketUrl: string = API_CONFIG.bucketBaseUrl;
+
+  items: CategoriaDTO[];
+
   constructor(public navCtrl: NavController, public navParams: NavParams, public categoriaService: CategoriaService) {
   }
 
   ionViewDidLoad() {
     this.categoriaService.findAll()
-      .subscribe(response => {
-        console.log(response);
+      .subscribe(response  => {
+        this.items = response.result;
       }),
       error => {
         console.log(error);
