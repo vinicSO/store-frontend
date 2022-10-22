6d955566df25342d9410c5280f2709f86de828c0
Vinícius Santos <vinicius.santos.oliv3ira@gmail.com>
2022-10-21 21:30:02 -0300

    tratando erros 403

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,15 +1,42 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Rx";
+import { StorageService } from "../services/storage.service";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
+  constructor(public storage: StorageService) {
+
+  }
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req)
       .catch((error, caught) => {
-        return Observable.throw(error);
+
+        let errorObj = error;
+        if (errorObj.error) {
+          errorObj = errorObj.error
+        }
+        if (!errorObj.status) {
+          errorObj = JSON.parse(errorObj);
+        }
+
+        switch (errorObj.status) {
+          case 403:
+            this.handle403();
+            break;
+
+          default:
+            break;
+        }
+
+        return Observable.throw(errorObj);
       }) as any;
+  }
+
+  handle403() {
+    this.storage.setLocalUser(null);
   }
 }
 
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -29,8 +29,14 @@ export class ProfilePage {
           this.cliente = response["obj"];
           this.getImageIfExists();
         },
-        error => {}
+        error => {
+          if (error.status == 403) {
+            this.navCtrl.setRoot('HomePage');
+          }
+        }
       );
+    } else {
+      this.navCtrl.setRoot('HomePage');
     }
   }
 
