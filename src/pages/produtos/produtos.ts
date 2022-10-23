5e54bb307e4060ba492f0b3e6c191ae7a76a8981
Vinícius Santos <vinicius.santos.oliv3ira@gmail.com>
2022-10-23 19:41:39 -0300

    carregando imagem dos produtos

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
 
@@ -23,9 +24,15 @@ export class ProdutosPage {
       .subscribe(
         response => {
           this.items = response["obj"]["content"];
+          this.items.map(p => p.imageUrl=`${API_CONFIG.bucketBaseUrl}/prod${p.id}-small.jpg`);
+          console.log(this.items)
         },
         error => {}
       );
+  }
+
+  onImageError(item_id: string) {
+    this.items.find(p => p.id === item_id).imageUrl = 'assets/imgs/prod.png';
   }
 
 }
