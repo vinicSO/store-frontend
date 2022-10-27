b5a1ab6213c113e4a98c6874aa961b8888919f1d
Vinícius Santos <vinicius.santos.oliv3ira@gmail.com>
2022-10-27 20:48:04 -0300

    componente loading na listagem de produtos

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, LoadingController, NavController, NavParams } from 'ionic-angular';
 import { API_CONFIG } from '../../config/api.config';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
@@ -16,17 +16,23 @@ export class ProdutosPage {
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public produtoService: ProdutoService) {
+    public produtoService: ProdutoService,
+    public loadingCtrl: LoadingController
+  ) {
   }
 
   ionViewDidLoad() {
+    let loader = this.presentLoading();
     this.produtoService.findByCategoria(this.navParams.get("categoria_id"))
       .subscribe(
         response => {
           this.items = response["obj"]["content"];
           this.items.map(p => p.imageUrl=`${API_CONFIG.bucketBaseUrl}/prod${p.id}-small.jpg`);
         },
-        error => {}
+        error => {},
+        () => {
+          loader.dismiss();
+        }
       );
   }
 
@@ -41,6 +47,14 @@ export class ProdutosPage {
         produto_id: produto_id
       }
     );
+  }
+
+  presentLoading() {
+    let loader = this.loadingCtrl.create({
+      content: 'Carregando...'
+    });
+    loader.present();
+    return loader;
   }
 
 }
